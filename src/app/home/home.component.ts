545c3a359771621ac0cb6576e389b0db1bcaa61c
VEZENTAN Oana <oana.vezentan@soprasteria.com>
2017-11-09 14:03:00 +0100

    Created a test Node server, added form-poster service, added submit event and validation, posting with observable, handling error, retrieving a server generated ID, loading form data dynamically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {Employee} from '../models/employee.model';
+import { FormPoster } from '../services/form-poster.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -7,9 +9,29 @@ import {Employee} from '../models/employee.model';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  languages = ['English', 'Spanish', 'Other'];
+  languages = [];
   model = new Employee('', '', true, '', 'default');
   hasPrimaryLanguageError = false;
+
+  constructor(private formPoster: FormPoster) {
+    this.formPoster.getLanguages()
+      .subscribe(
+        data => this.languages = data.languages,
+        err => console.log('get error: ', err)
+      );
+  }
+
+  submitForm(form: NgForm) {
+    this.validatePrimaryLanguage(this.model.primaryLanguage);
+    if(this.hasPrimaryLanguageError) {
+      return;
+    }
+    this.formPoster.postEmployeeForm(this.model)
+      .subscribe (
+        data => console.log('success:', data),
+        err => console.log('error:', err)
+      )
+  }
 
   validatePrimaryLanguage(value) {
     if ( value === 'default') {
